2dc0441e1d77f798622164e6f7a9f98c8361add0
Chris Abrams <mail@chrisabrams.com>
2022-05-09 13:21:50 -0500

    add enemy type

--- Harvard_Action1/Assets/Scripts/NPCs/EnemyMeleeDamage.cs
+++ Harvard_Action1/Assets/Scripts/NPCs/EnemyMeleeDamage.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyMeleeDamage : MonoBehaviour {
        private Renderer rend;
        public Animator anim;
+       public EnemyType enemyType;
        public GameObject healthLoot;
        public int maxHealth = 10;
        public int currentHealth;
@@ -25,34 +26,39 @@ public class EnemyMeleeDamage : MonoBehaviour {
               //anim.SetTrigger ("Hurt");
               Debug.LogFormat("Dragon Health: {0} {1}", damage, currentHealth);
               if (currentHealth <= 0){
-				 // anim.SetBool ("isDead", true);
-				  
-			Die();
-			//GetComponent<AudioSource>().Play();
+		       // anim.SetBool ("isDead", true);
 			enemyDieSFX.Play();
-					
-		       //Debug.Log("camera shake");
+                     Die();
               }
        }
 
-       void Die(){
+       void Die() {
 		   
               Instantiate (healthLoot, transform.position, Quaternion.identity);
               
 			  //Debug.Log("isDead=",anim);
               GetComponent<Collider2D>().enabled = false;
-			  cameraShake.ShakeCamera(0.15f,0.3f);
+		cameraShake.ShakeCamera(0.15f,0.3f);
+
               StartCoroutine(Death());
        }
 
-       IEnumerator Death(){
+       IEnumerator Death() {
               yield return new WaitForSeconds(1f);
               //Debug.Log("You Killed a baddie. You deserve loot!");
-			  Destroy(gameObject);
+		Destroy(gameObject);
        }
 
        IEnumerator ResetColor(){
               yield return new WaitForSeconds(1f);
               rend.material.color = Color.white;
        }
+}
+
+public enum EnemyType {
+       DragonBaby,
+       DragonLarge,
+       JellyRed,
+       JellyWhite,
+       JellyYellow,
 }
