ad934ce6eb0d850a773b5f9d024d8ed33fa1191d
sam3364 <98614972+sam3364@users.noreply.github.com>
2022-05-11 23:14:13 +0300

    sign says jelly dragon has key

--- Harvard_Action1/Assets/Scripts/DoorExit_1.cs
+++ Harvard_Action1/Assets/Scripts/DoorExit_1.cs
@@ -8,20 +8,29 @@ public class DoorExit_1 : MonoBehaviour{
 
     public string nextLevel = "scene4_RooftopJellyDragonBoss_Shared";
 	public AudioSource doorSFX;
+	public GameHandler gameHandler;
+	public int playercolor;
 	
 
     public void Start(){
       //       audioManager = GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>();
 		doorSFX = GetComponent<AudioSource>();
+		  gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
 	}         
-      
+      public void Update(){
+		  playercolor=gameHandler.playercolor;
+	  }
 	  public void OnTriggerEnter2D(Collider2D other){
             // AudioManager audioManager;
             // audioManager = GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>();
 			//   audioManager.PlaySound("opendoor");
             if (other.gameObject.tag == "Player"){
+				if(playercolor==2){
 				
 							SceneManager.LoadScene(nextLevel); 
             }
-      }	 
+			
+				
+      }	
+	  }	  
 }
