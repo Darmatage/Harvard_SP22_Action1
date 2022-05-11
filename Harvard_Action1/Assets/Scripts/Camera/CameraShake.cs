124576c7c2235c6d9ff3417e02dbff1fdf535814
Chris Abrams <mail@chrisabrams.com>
2022-05-11 17:02:26 -0500

    camera changes based on dragon

--- Harvard_Action1/Assets/Scripts/Camera/CameraShake.cs
+++ Harvard_Action1/Assets/Scripts/Camera/CameraShake.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraShake : MonoBehaviour{
-
+public class CameraShake : MonoBehaviour {
+       public new Camera camera;
+       public CameraLevel cameraLevel;
        public float durationTime = 0.15f;
        public float magnitude = 0.5f;
 	public GameObject target;
        public float camSpeed = 4.0f;
-	  
-	void Start(){
-            target = GameObject.FindWithTag("Player");
+       public GameObject jellyDragon;
+       public EnemyMeleeDamage jellyDragonEMD;
+       public int jellyDragonHP;
+
+	void Start() {
+              camera = Camera.main;
+
+              if (cameraLevel == CameraLevel.Four) {
+                     jellyDragon = GameObject.FindWithTag("JellyDragon");
+                     jellyDragonEMD = jellyDragon.GetComponent<EnemyMeleeDamage>();
+              }
+
+              target = GameObject.FindWithTag("Player");
        }
 	void FixedUpdate () {
             Vector2 pos = Vector2.Lerp ((Vector2)transform.position, (Vector2)target.transform.position, camSpeed * Time.fixedDeltaTime);
@@ -18,10 +29,26 @@ public class CameraShake : MonoBehaviour{
        }
 
        //The Update Function is just for testing (hit [p]), and can be commented out:
-       void Update(){
+       void Update() {
              // if (Input.GetKeyDown(KeyCode.P)){
                    //  StartCoroutine(ShakeMe(durationTime,magnitude));
             // }
+
+              try {
+                     if (cameraLevel == CameraLevel.Four) {
+                            jellyDragonHP = jellyDragonEMD.currentHealth;
+
+                            if (jellyDragonHP > 0) {
+                                   camera.orthographicSize = 10f;
+                            }
+                            else {
+                                   camera.orthographicSize = 5f;
+                            }
+                     }
+              }
+              catch {
+                     camera.orthographicSize = 5f;
+              }
        }
 
        //use this to call from another script, like when the player gets hit
@@ -46,4 +73,11 @@ public class CameraShake : MonoBehaviour{
               transform.localPosition = origPos;
        }
 
+}
+
+public enum CameraLevel {
+       One,
+       Two,
+       Three,
+       Four
 }
