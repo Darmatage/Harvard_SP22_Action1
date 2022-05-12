cba42750e83425e60021fe4014405a2b1f3b2efb
Chris Abrams <mail@chrisabrams.com>
2022-05-12 13:41:15 -0500

    fix fall check

--- Harvard_Action1/Assets/Scripts/Game/GameHandler.cs
+++ Harvard_Action1/Assets/Scripts/Game/GameHandler.cs
@@ -29,6 +29,7 @@ public class GameHandler : MonoBehaviour {
 	public GameObject StartButton;
 	
 	public GameObject QuitButton;
+      private new Transform transform;
 
       void Awake () {
             SetAudioLevel (volumeLevel);
@@ -53,6 +54,7 @@ public class GameHandler : MonoBehaviour {
 
             player = GameObject.FindWithTag("Player");
             bars = player.GetComponent<PlayerBars>();
+            transform = player.GetComponent<Transform>();
 			
             DefineGameLevels();
             SetGameLevel(1); // Start the game at level 1
@@ -87,7 +89,7 @@ public class GameHandler : MonoBehaviour {
 
       // Are we on a game level or a cutscene / menu
       bool IsGameLevel() {
-            if (player) {
+            if (GameObject.FindWithTag("Player") != null) {
                   return true;
             }
 
@@ -96,9 +98,7 @@ public class GameHandler : MonoBehaviour {
 
       // Has player fallen off map?
       void FallCheck() {
-           Transform transform = player.GetComponent<Transform>();
-
-           if (transform.position.y <= -10) {
+            if (transform.position.y <= -10) {
                   Debug.Log("Fell Off");
 
                   SetPlayerPosition(currentLevel.spawnPoint);
